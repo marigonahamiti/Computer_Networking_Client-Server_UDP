daa0f7389fa5172f96ea448097db52f86041e8fb
MarigonaHamiti <115891512+marigonahamiti@users.noreply.github.com>
2023-11-28 14:07:47 +0100

    Update client.cs

--- client.cs
+++ client.cs
@@ -1,1 +1,38 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class UDPClient
+{
+    static void Main()
+    {
+        string serverName = "localhost";
+        int serverPort = 1200;
+
+        for (int i = 0; i < 5; i++)
+        {
+            using (UdpClient clientS = new UdpClient())
+            {
+                while (true)
+                {
+                    Console.Write("Enter a message to send to the server (or 'exit' to quit): ");
+                    string message = Console.ReadLine();
+
+                    if (message.ToLower() == "exit")
+                        break;
+
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+                    clientS.Send(data, data.Length, serverName, serverPort);
+
+                    IPEndPoint serverAddress = new IPEndPoint(IPAddress.Any, 0);
+                    byte[] receivedData = clientS.Receive(ref serverAddress);
+                    string modifiedMessage = Encoding.UTF8.GetString(receivedData);
+
+                    Console.WriteLine("Response from the server: " + modifiedMessage);
+                }
+            }
+        }
+    }
+}
 
