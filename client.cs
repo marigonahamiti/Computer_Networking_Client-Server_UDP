f203fedb527ccc8a0449ada0b43991313815a9e5
Medina1 <121581992+Mediinnaa@users.noreply.github.com>
2023-12-02 10:37:53 +0100

    Update client.cs

--- client.cs
+++ client.cs
@@ -57,9 +57,10 @@ class UDPClient
                     Console.WriteLine("2. Read a file");
                     Console.WriteLine("3. Write to a file");
                     Console.WriteLine("4. Execute a file");
-                    Console.WriteLine("5. Exit");
+                    Console.WriteLine("5. Client with full access");
+                    Console.WriteLine("6. Exit");
 
-                    Console.Write("Enter your choice (1, 2, 3, 4, or 5): ");
+                  Console.Write("Enter your choice (1, 2, 3, 4, 5, or 6): ");
                     string choice = Console.ReadLine();
 
                     if (choice == "1")
@@ -88,82 +89,88 @@ class UDPClient
                             Console.WriteLine(errorFile);
                             byte[] data = Encoding.UTF8.GetBytes(errorFile);
                             clientS.Send(data, data.Length, serverName, serverPort);
-
                         }
                     }
                     else if (choice == "3")
                     {
                         Console.Write("Enter the file name to write (e.g., newfile.txt): ");
                         string fileName = Console.ReadLine();
-                        string filePath = Path.Combine(@"C:\Users\milot\Computer_Networking_Gr24\", fileName);
-
                         Console.Write("Enter the content for the file: ");
                         string fileContent = Console.ReadLine();
 
-                        File.WriteAllText(filePath, fileContent);
-                        byte[] data = Encoding.UTF8.GetBytes($"WRITE:{fileName}");
+                        if (fileName != null)
+                        {
+                            string filePath = Path.Combine(@"C:\Users\milot\Computer_Networking_Gr24\", fileName);
+                            File.WriteAllText(filePath, fileContent);
+
+                            byte[] data = Encoding.UTF8.GetBytes($"WRITE:{fileName}");
+                            clientS.Send(data, data.Length, serverName, serverPort);
+
+                            // Wait for acknowledgment from the server
+                            string fileContent1 = File.ReadAllText(filePath);
+                            byte[] data1 = Encoding.UTF8.GetBytes(fileContent1);
+                            clientS.Send(data1, data1.Length, serverName, serverPort);
+                        }
+                    }
+                    else if (choice == "4")
+                    {
+                        Console.WriteLine("Choose a command to execute:");
+                        Console.WriteLine("1. mkdr [directory_name]");
+                        Console.WriteLine("2. ls");
+
+                        Console.Write("Enter your choice (1 or 2): ");
+                        string subChoice = Console.ReadLine();
+
+                        string command = "";
+                        if (subChoice == "1")
+                        {
+                            Console.Write("Enter the directory name to create: ");
+                            string dirName = Console.ReadLine();
+                            command = $"EXECUTE:mkdr {dirName}";
+                        }
+                        else if (subChoice == "2")
+                        {
+                            command = "EXECUTE:ls";
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid sub-choice. Please enter 1 or 2.");
+                            continue; // Skip the rest of the loop iteration
+                        }
+
+                        byte[] data = Encoding.UTF8.GetBytes(command);
                         clientS.Send(data, data.Length, serverName, serverPort);
 
-                        // Wait for acknowledgment from the server
-                        string fileContent1 = File.ReadAllText(filePath);
-                        byte[] data1 = Encoding.UTF8.GetBytes(fileContent1);
-                        clientS.Send(data1, data1.Length, serverName, serverPort);
-                        // The break statement is removed here
+                        receivedData = clientS.Receive(ref serverAddress);
+                        string response = Encoding.UTF8.GetString(receivedData);
+
+                        Console.WriteLine("Response from the server: ");
+                        Console.WriteLine(response);
                     }
-
-            
-
-                    else if (choice == "4")
-                {
-                    Console.WriteLine("Choose a command to execute:");
-                    Console.WriteLine("1. mkdr [directory_name]");
-                    Console.WriteLine("2. ls");
-
-                    Console.Write("Enter your choice (1 or 2): ");
-                    string subChoice = Console.ReadLine();
-
-                    string command = "";
-                    if (subChoice == "1")
+                    else if (choice == "5")
                     {
-                        Console.Write("Enter the directory name to create: ");
-                        string dirName = Console.ReadLine();
-                        command = $"EXECUTE:mkdr {dirName}";
+                        // Code for Client with full access
+                        Console.Write("Enter the server IP address to request full access: ");
+                        string serverIpAddress = Console.ReadLine();
+                        if (serverIpAddress != null)
+                        {
+                            string requestFullAccessMessage = $"GRANT_FULL_ACCESS:{serverIpAddress}";
+                            byte[] data = Encoding.UTF8.GetBytes(requestFullAccessMessage);
+                            clientS.Send(data, data.Length, serverName, serverPort);
+                        }
                     }
-                    else if (subChoice == "2")
-                    {
-                        command = "EXECUTE:ls";
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid sub-choice. Please enter 1 or 2.");
-                        continue; // Skip the rest of the loop iteration
-                    }
-
-                    byte[] data = Encoding.UTF8.GetBytes(command);
-                    clientS.Send(data, data.Length, serverName, serverPort);
-
-                    receivedData = clientS.Receive(ref serverAddress);
-                    string response = Encoding.UTF8.GetString(receivedData);
-
-                    Console.WriteLine("Response from the server: ");
-                    Console.WriteLine(response);
-                }
-
-                       else if (choice == "5")
+                    else if (choice == "6")
                     {
                         Console.WriteLine("Exiting the client.");
                         break;
-                    } 
-
+                    }
                     else
                     {
-                        Console.WriteLine("Invalid choice. Please enter 1, 2, 3, 4, or 5.");
+                        Console.WriteLine("Invalid choice. Please enter 1, 2, 3, 4, 5, or 6.");
                     }
 
                     receivedData = clientS.Receive(ref serverAddress);
                     string modifiedMessage = Encoding.UTF8.GetString(receivedData);
-
-                    Console.WriteLine("Response from the server: " + modifiedMessage);
                 }
             }
         }
