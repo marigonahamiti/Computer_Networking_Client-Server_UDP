0c663a62f69666bf00c76970900339c47dffddc6
Medina1 <121581992+Mediinnaa@users.noreply.github.com>
2023-12-03 21:50:31 +0100

    Update client.cs

--- client.cs
+++ client.cs
@@ -6,14 +6,14 @@ using System.Text;
 
 class UDPClient
 {
-    static IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1200);
+    // static IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1200);
 
     static void Main()
     {
-        string serverName = "localhost";
+        string serverName = "192.168.0.23"; // Update this with the server's IP address
         int serverPort = 1200;
 
-        IPEndPoint serverAddress = new IPEndPoint(IPAddress.Any, 0);
+        IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse(serverName), serverPort);
         byte[] receivedData;
 
         Console.WriteLine("Choose the type of connection:");
@@ -62,183 +62,183 @@ class UDPClient
                         bool hasFullAccess = connectionResponse.Contains("FULL_ACCESS");
 
 
-                    while(true)
-                    {
-                        if (hasFullAccess)
+                        while (true)
                         {
-                            
-                            Console.WriteLine("Choose an option:");
-                            Console.WriteLine("1. Enter a message");
-                            Console.WriteLine("2. Read a file");
-                            Console.WriteLine("3. Write to a file");
-                            Console.WriteLine("4. Execute a command");
-                            Console.WriteLine("5. Exit");  
-                        }
-                    
-                        else if(!hasFullAccess)
-                        {
-                            Console.WriteLine("Choose an option:");
-                            Console.WriteLine("1. Enter a message");
-                            Console.WriteLine("2. Read a file");
-                            Console.WriteLine("3. Exit");
-                        }
-
-                        Console.Write("Enter your choice: ");
-                        string choice = Console.ReadLine();
-
-                        // Handle client options based on the choice
-                        // (Note: You may need to adjust the logic based on your specific requirements.)
-
-                        if (hasFullAccess)
-                        { 
-
-                            // Handle options for clients with full access
-                            if (choice == "1")
+                            if (hasFullAccess)
                             {
-                                Console.Write("Enter a message to send to the server: ");
-                                string message = Console.ReadLine();
 
-                                byte[] data = Encoding.UTF8.GetBytes(message);
-                                clientS.Send(data, data.Length, serverName, serverPort);
+                                Console.WriteLine("Choose an option:");
+                                Console.WriteLine("1. Enter a message");
+                                Console.WriteLine("2. Read a file");
+                                Console.WriteLine("3. Write to a file");
+                                Console.WriteLine("4. Execute a command");
+                                Console.WriteLine("5. Exit");
                             }
-                              else if (choice == "2")
-                            {
-                                Console.Write("Enter the file name (e.g., hello.txt): ");
-                                string fileName = Console.ReadLine();
-                                string filePath = Path.Combine(@"C:\Users\ZoneTech\Desktop\Detyra2\", fileName);
 
-                                if (File.Exists(filePath))
+                            else if (!hasFullAccess)
+                            {
+                                Console.WriteLine("Choose an option:");
+                                Console.WriteLine("1. Enter a message");
+                                Console.WriteLine("2. Read a file");
+                                Console.WriteLine("3. Exit");
+                            }
+
+                            Console.Write("Enter your choice: ");
+                            string choice = Console.ReadLine();
+
+                            // Handle client options based on the choice
+                            // (Note: You may need to adjust the logic based on your specific requirements.)
+
+                            if (hasFullAccess)
+                            {
+
+                                // Handle options for clients with full access
+                                if (choice == "1")
                                 {
-                                    string fileContent = File.ReadAllText(filePath);
-                                    byte[] data = Encoding.UTF8.GetBytes(fileContent);
+                                    Console.Write("Enter a message to send to the server: ");
+                                    string message = Console.ReadLine();
+
+                                    byte[] data = Encoding.UTF8.GetBytes(message);
                                     clientS.Send(data, data.Length, serverName, serverPort);
+                                }
+                                else if (choice == "2")
+                                {
+                                    Console.Write("Enter the file name (e.g., hello.txt): ");
+                                    string fileName = Console.ReadLine();
+                                    string filePath = Path.Combine(@"C:\Users\ZoneTech\Desktop\Detyra2\", fileName);
+
+                                    if (File.Exists(filePath))
+                                    {
+                                        string fileContent = File.ReadAllText(filePath);
+                                        byte[] data = Encoding.UTF8.GetBytes(fileContent);
+                                        clientS.Send(data, data.Length, serverName, serverPort);
+                                    }
+                                    else
+                                    {
+                                        string errorFile = $"File '{fileName}' not found";
+                                        Console.WriteLine(errorFile);
+                                        byte[] data = Encoding.UTF8.GetBytes(errorFile);
+                                        clientS.Send(data, data.Length, serverName, serverPort);
+                                    }
+                                }
+                                else if (choice == "3")
+                                {
+                                    Console.Write("Enter the file name to write (e.g., newfile.txt): ");
+                                    string fileName = Console.ReadLine();
+                                    string filePath = Path.Combine(@"C:\Users\ZoneTech\Desktop\Detyra2\", fileName);
+
+                                    Console.Write("Enter the content for the file: ");
+                                    string fileContent = Console.ReadLine();
+
+                                    File.WriteAllText(filePath, fileContent);
+                                    byte[] data = Encoding.UTF8.GetBytes($"WRITE:{fileName}");
+                                    clientS.Send(data, data.Length, serverAddress);
+
+                                    // Wait for confirmation from the server
+                                    receivedData = clientS.Receive(ref serverAddress);
+                                    string confirmationMessage = Encoding.UTF8.GetString(receivedData);
+
+                                    Console.WriteLine("Server response: " + confirmationMessage);
+                                }
+                                else if (choice == "4")
+                                {
+                                    Console.WriteLine("Choose a command to execute:");
+                                    Console.WriteLine("1. mkdr [directory_name]");
+                                    Console.WriteLine("2. ls");
+
+                                    Console.Write("Enter your choice (1 or 2): ");
+                                    string subChoice = Console.ReadLine();
+
+                                    string command = "";
+                                    if (subChoice == "1")
+                                    {
+                                        Console.Write("Enter the directory name to create: ");
+                                        string dirName = Console.ReadLine();
+                                        command = $"EXECUTE:mkdr {dirName}";
+                                    }
+                                    else if (subChoice == "2")
+                                    {
+                                        command = "EXECUTE:ls";
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Invalid sub-choice. Please enter 1 or 2.");
+                                        continue; // Skip the rest of the while loop iteration
+                                    }
+
+                                    byte[] data = Encoding.UTF8.GetBytes(command);
+                                    clientS.Send(data, data.Length, serverAddress);
+
+                                    // Wait for the response from the server
+                                    receivedData = clientS.Receive(ref serverAddress);
+                                    string response = Encoding.UTF8.GetString(receivedData);
+
+                                    Console.WriteLine("Server response: ");
+                                    Console.WriteLine(response);
+                                }
+                                else if (choice == "5")
+                                {
+                                    Console.WriteLine("Exiting the client.");
+                                    return;
                                 }
                                 else
                                 {
-                                    string errorFile = $"File '{fileName}' not found";
-                                    Console.WriteLine(errorFile);
-                                    byte[] data = Encoding.UTF8.GetBytes(errorFile);
+                                    Console.WriteLine("Invalid choice. Please enter a number between 1 and 5.");
+                                }
+                            }
+                            else if (!hasFullAccess)
+                            {
+                                // Handle options for clients without full access
+                                if (choice == "1")
+                                {
+                                    Console.Write("Enter a message to send to the server: ");
+                                    string message = Console.ReadLine();
+
+                                    byte[] data = Encoding.UTF8.GetBytes(message);
                                     clientS.Send(data, data.Length, serverName, serverPort);
                                 }
-                            }
-                            else if (choice == "3")
-                            {
-                                Console.Write("Enter the file name to write (e.g., newfile.txt): ");
-                                string fileName = Console.ReadLine();
-                                string filePath = Path.Combine(@"C:\Users\ZoneTech\Desktop\Detyra2\", fileName);
-
-                                Console.Write("Enter the content for the file: ");
-                                string fileContent = Console.ReadLine();
-
-                                File.WriteAllText(filePath, fileContent);
-                                byte[] data = Encoding.UTF8.GetBytes($"WRITE:{fileName}");
-                                clientS.Send(data, data.Length, serverAddress);
-
-                                // Wait for confirmation from the server
-                                receivedData = clientS.Receive(ref serverAddress);
-                                string confirmationMessage = Encoding.UTF8.GetString(receivedData);
-
-                                Console.WriteLine("Server response: " + confirmationMessage);
-                            }
-                            else if (choice == "4")
-                            {
-                                Console.WriteLine("Choose a command to execute:");
-                                Console.WriteLine("1. mkdr [directory_name]");
-                                Console.WriteLine("2. ls");
-
-                                Console.Write("Enter your choice (1 or 2): ");
-                                string subChoice = Console.ReadLine();
-
-                                string command = "";
-                                if (subChoice == "1")
+                                else if (choice == "2")
                                 {
-                                    Console.Write("Enter the directory name to create: ");
-                                    string dirName = Console.ReadLine();
-                                    command = $"EXECUTE:mkdr {dirName}";
+                                    Console.Write("Enter the file name (e.g., hello.txt): ");
+                                    string fileName = Console.ReadLine();
+                                    string filePath = Path.Combine(@"C:\Users\ZoneTech\Desktop\Detyra2\", fileName);
+
+                                    if (File.Exists(filePath))
+                                    {
+                                        string fileContent = File.ReadAllText(filePath);
+                                        byte[] data = Encoding.UTF8.GetBytes(fileContent);
+                                        clientS.Send(data, data.Length, serverName, serverPort);
+                                    }
+                                    else
+                                    {
+                                        string errorFile = $"File '{fileName}' not found";
+                                        Console.WriteLine(errorFile);
+                                        byte[] data = Encoding.UTF8.GetBytes(errorFile);
+                                        clientS.Send(data, data.Length, serverName, serverPort);
+                                    }
                                 }
-                                else if (subChoice == "2")
+                                else if (choice == "3")
                                 {
-                                    command = "EXECUTE:ls";
+                                    Console.WriteLine("Exiting the client.");
+                                    return;
                                 }
                                 else
                                 {
-                                    Console.WriteLine("Invalid sub-choice. Please enter 1 or 2.");
-                                    continue; // Skip the rest of the while loop iteration
+                                    Console.WriteLine("Invalid choice. Please enter a number between 1 and 3.");
                                 }
-
-                                byte[] data = Encoding.UTF8.GetBytes(command);
-                                clientS.Send(data, data.Length, serverAddress);
-
-                                // Wait for the response from the server
-                                receivedData = clientS.Receive(ref serverAddress);
-                                string response = Encoding.UTF8.GetString(receivedData);
-
-                                Console.WriteLine("Server response: ");
-                                Console.WriteLine(response);
                             }
-                            else if (choice == "5")
-                            {
-                                Console.WriteLine("Exiting the client.");
-                                return;
-                            }
-                            else
-                            {
-                                Console.WriteLine("Invalid choice. Please enter a number between 1 and 5.");
-                            }
+
+                            // Receive and display the response from the server
+                            byte[] choiceData = Encoding.UTF8.GetBytes(choice);
+                            clientS.Send(choiceData, choiceData.Length, serverAddress);
+
+                            // Receive and display the response from the server
+                            receivedData = clientS.Receive(ref serverAddress);
+                            string modifiedMessage = Encoding.UTF8.GetString(receivedData);
+
+                            Console.WriteLine("Response from the server: " + modifiedMessage);
                         }
-                        else if(!hasFullAccess)
-                        {
-                            // Handle options for clients without full access
-                            if (choice == "1")
-                            {
-                                Console.Write("Enter a message to send to the server: ");
-                                string message = Console.ReadLine();
-
-                                byte[] data = Encoding.UTF8.GetBytes(message);
-                                clientS.Send(data, data.Length, serverName, serverPort);
-                            }
-                            else if (choice == "2")
-                            {
-                                Console.Write("Enter the file name (e.g., hello.txt): ");
-                                string fileName = Console.ReadLine();
-                                string filePath = Path.Combine(@"C:\Users\ZoneTech\Desktop\Detyra2\", fileName);
-
-                                if (File.Exists(filePath))
-                                {
-                                    string fileContent = File.ReadAllText(filePath);
-                                    byte[] data = Encoding.UTF8.GetBytes(fileContent);
-                                    clientS.Send(data, data.Length, serverName, serverPort);
-                                }
-                                else
-                                {
-                                    string errorFile = $"File '{fileName}' not found";
-                                    Console.WriteLine(errorFile);
-                                    byte[] data = Encoding.UTF8.GetBytes(errorFile);
-                                    clientS.Send(data, data.Length, serverName, serverPort);
-                                }
-                            }
-                            else if (choice == "3")
-                            {
-                                Console.WriteLine("Exiting the client.");
-                                return;
-                            }
-                            else
-                            {
-                                Console.WriteLine("Invalid choice. Please enter a number between 1 and 3.");
-                            }
-                        }
-
-                        // Receive and display the response from the server
-                        byte[] choiceData = Encoding.UTF8.GetBytes(choice);
-                        clientS.Send(choiceData, choiceData.Length, serverAddress);
-
-                        // Receive and display the response from the server
-                        receivedData = clientS.Receive(ref serverAddress);
-                        string modifiedMessage = Encoding.UTF8.GetString(receivedData);
-
-                        Console.WriteLine("Response from the server: " + modifiedMessage);
                     }
-                }
                 }
                 catch (Exception ex)
                 {
@@ -248,3 +248,4 @@ class UDPClient
         }
     }
 }
+
